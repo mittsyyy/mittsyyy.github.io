4e47ca62a9aa8ab6387e18aa80dd31926a788ee6
Yuritsyyy1 <23cg0281@itsncg.edu.mx>
2025-10-20 21:20:54 -0600

    Menú
    Agregue el menú de hamburguesa en la página de ofertas

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+const openMenuBtn = document.getElementById('open-menu-btn'); 
+
+const menuLateral = document.getElementById('menu-lateral');
+
+const closeMenuBtn = document.getElementById('close-menu-btn');
+
+openMenuBtn.addEventListener('click', () => {
+    menuLateral.classList.toggle('abierto');
+});
+if (closeMenuBtn) {
+    closeMenuBtn.addEventListener('click', () => {
+        menuLateral.classList.remove('abierto');
+    });
+}
