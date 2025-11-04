8c4319eb8ed070cba248eb5513b34ef75de9d689
Yuritsyyy1 <23cg0281@itsncg.edu.mx>
2025-11-03 20:01:54 -0600

    detalles

--- js/detalles.js
+++ js/detalles.js
@@ -2,7 +2,7 @@ window.onload = ()=>{
     let imgGaleria =document.querySelector("#main-product-img")
     let imgs=document.querySelectorAll(".thumb")
     let btn=document.querySelector(".active")
-    let btns=document.querySelector("")
+    let btnsize=document.querySelectorAll(".size-btn")
     for(let i=0;i<imgs.length;i++){
         imgs[i].addEventListener('click',(evt)=>{
             console.log(evt.target)
@@ -12,6 +12,16 @@ window.onload = ()=>{
                 item.classList.remove('active')
             })
             evt.target.classList.add('active')
+        })
+    }
+    for(let i= 0;i<btnsize.length;i++){
+        btnsize[i].addEventListener('click',(evt)=>{
+            btnsize.forEach(item=>{
+                item.classList.remove('active')
+            });
+             evt.target.classList.add('active');
+ 
+            console.log("Tamaño seleccionado:", evt.target.txtContent);
         })
     }
 }
--- js/script.js
+++ js/script.js
@@ -13,6 +13,5 @@ if (closeMenuBtn) {
         menuLateral.classList.remove('abierto');
     });
 }
-
 }
 
